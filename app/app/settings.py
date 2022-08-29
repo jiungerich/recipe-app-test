a9ebdeb4fee7f7c1463108f620e06c31365ea6f7
jiungerich <jiungerich@users.noreply.github.com>
2022-08-29 13:17:48 -0700

    added user model and migrated

--- app/app/settings.py
+++ app/app/settings.py
@@ -82,7 +82,6 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASS'),
-        #'PORT': os.environ.get('DB_PORT'),
     }
 }
 
@@ -129,3 +128,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
